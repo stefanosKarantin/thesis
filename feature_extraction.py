bf6c3658bc2a73d8ad18f5cda206741fcc9ffc08
stefanosKarantin <karantinakis.s@gmail.com>
2017-03-12 14:47:40 +0200

    adjust data-arrays for the new db

--- feature_extraction.py
+++ feature_extraction.py
@@ -8,39 +8,22 @@ import numpy
 from get_features import get_feature_table
 
 anger = []
-disgust = []
-fear = []
-happiness = []
+emphatic = []
 neutral = []
-pain = []
-pleasure = []
-sadness = []
-surprise = []
+motherese = []
 
-pathname = 'emotional_db/'
+pathname = 'aibo-data-train/'
 for filename in os.listdir(pathname):
 	if filename.endswith('.wav') or filename.endswith('.mp3'):
-		if fnmatch.fnmatch(filename,'*anger*'):
+		if fnmatch.fnmatch(filename,'*A*'):
 			anger.append(get_feature_table(pathname+filename))
-		elif fnmatch.fnmatch(filename,'*disgust*'):
-			disgust.append(get_feature_table(pathname+filename))
-		elif fnmatch.fnmatch(filename,'*fear*'):
-			fear.append(get_feature_table(pathname+filename))
-		elif fnmatch.fnmatch(filename,'*happiness*'):
-			happiness.append(get_feature_table(pathname+filename))
-		elif fnmatch.fnmatch(filename,'*neutral*'):
+		elif fnmatch.fnmatch(filename,'*E*'):
+			emphatic.append(get_feature_table(pathname+filename))
+		elif fnmatch.fnmatch(filename,'*N*'):
 			neutral.append(get_feature_table(pathname+filename))
-		elif fnmatch.fnmatch(filename,'*pain*'):
-			pain.append(get_feature_table(pathname+filename))
-		elif fnmatch.fnmatch(filename,'*pleasure*'):
-			pleasure.append(get_feature_table(pathname+filename))
-		elif fnmatch.fnmatch(filename,'*sadness*'):
-			sadness.append(get_feature_table(pathname+filename))
-		elif fnmatch.fnmatch(filename,'*surprise*'):
-			surprise.append(get_feature_table(pathname+filename))
+		elif fnmatch.fnmatch(filename,'*M*'):
+			motherese.append(get_feature_table(pathname+filename))
 
+training_set = {'anger': anger, 'emphatic': emphatic, 'neutral': neutral, 'motherese': motherese}
 
-training_set = {'anger': anger, 'disgust': disgust, 'fear': fear, 'happiness': happiness, 'neutral': neutral, 'pain': pain, 'pleasure': pleasure, 'sadness': sadness, 'surprise': surprise}
-
-#print training_set
-        
+print anger
